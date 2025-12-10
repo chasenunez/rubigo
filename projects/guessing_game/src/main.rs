a1dc49bda096335a4ba8b95837c35ca105fe7020
Chase Núñez <chasenunez@gmail.com>
2025-12-10 13:54:45 +0100

    final program finished

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -4,27 +4,37 @@ use std::io; // importing the input/output library into the scope of this progra
 use rand::Rng;
 
 fn main() { // no parameters needed
-    println!("Errate die Zahl!"); // guess the number
+    println!("Bitte errate die Zahl"); // guess the number
 
     let secret_number = rand::thread_rng().gen_range(1..=100); //gives us the particular random number generator we’re going to use: one that is local to the current thread of execution and is seeded by the operating system
                                                                 // gen_range method takes a range expression as an argument and generates a random number in the range
-    println!("Die Geheimzahl ist: {secret_number}");
+    // println!("Die Geheimzahl ist: {secret_number}");
 
-    println!("Bitte geben Sie Ihre Vermutung ein."); // please input your guess
+    loop {
 
-    let mut guess = String::new();  // creating a mutable varaible named guess, which is a new empty string
+        println!("Bitte geben Sie Ihre Vermutung ein"); // please input your guess
 
-    io::stdin() //a handle to the standard input for the terminal
-        .read_line(&mut guess) //handles the user input, needs to be mutable so the  method can change the string's content
-                              // & indicates that this argument is a reference, which gives you a way to let multiple parts of your code access one piece of data without needing to copy that data into memory multiple times
-        .expect("Zeile konnte nicht gelesen werden"); //takes the enum result from above. if `err` and not `ok`, this error will be shown
+        let mut guess = String::new();  // creating a mutable varaible named guess, which is a new empty string
 
-    println!("Sie haben geraten: {guess}");
+        io::stdin() //a handle to the standard input for the terminal
+            .read_line(&mut guess) //handles the user input, needs to be mutable so the  method can change the string's content
+                                  // & indicates that this argument is a reference, which gives you a way to let multiple parts of your code access one piece of data without needing to copy that data into memory multiple times
+            .expect("Zeile konnte nicht gelesen werden"); //takes the enum result from above. if `err` and not `ok`, this error will be shown
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Zu klein!"),
-        Ordering::Greater => println!("Zu groß!"), 
-        Ordering::Equal => println!("Sie gewinnen!"), 
+        println!("Sie haben geraten: {guess}");
 
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Zu klein"),
+            Ordering::Greater => println!("Zu groß"), 
+            Ordering::Equal => {
+                println!("Sie gewinnen!");
+                break;
+            } 
+        }
     }
 }
